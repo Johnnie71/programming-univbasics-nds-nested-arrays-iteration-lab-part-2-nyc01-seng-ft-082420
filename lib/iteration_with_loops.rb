709b41f678f94bb18cad1674c96b560235de5ee9
Johnnie Gonzalez <johnnie.agonz71@gmail.com>
2020-08-05 19:40:17 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -16,20 +16,3 @@ def find_min_in_nested_arrays(src)
   minimum_temp
 end
 
-# def find_min_in_nested_arrays(src)
-#   # array_of_daily_temp = []
-#   row_index = 0
-#     while row_index < src.count do 
-#       element_index = 0
-#       min_integer_element = 100
-#         while element_index < src[row_index].count do 
-#           if src[row_index][element_index] < min_integer_element
-#             min_integer_element = src[row_index][element_index]
-#           end
-#           element_index += 1
-#         end
-#       array_of_daily_temp << min_integer_element
-#       row_index += 1
-#     end
-#     array_of_daily_temp
-# end
